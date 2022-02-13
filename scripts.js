4c2c177f80639f0f606079eaa6e418ba0d581c52
Mark Lawson <mark.lawson@cleveranchor.com>
2022-02-13 18:40:42 -0500

    added scripts.js FindInGrid.html

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
 //scripts for problems
+function resetFindInGrid() {
+    //set tbNumberToFind to blank
+    let tbNumberToFind = document.getElementById("tbNumberToFind");
+    tbNumberToFind.value = '';
+
+    //unhighlight spans
+    let spans = document.getElementsByTagName('span');
+    console.log("spans: ", spans);
+    for (let index = 0; index < spans.length; index++) {
+        spans[index].classList.remove('spanHighlighted');
+    }
+}
+
+
+function findInGrid() {
+ //find in grid
+}
